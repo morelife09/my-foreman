bd7a9632cf6446869d935b87d3c954d4b7309aa9
morelife09 <junichi1995930@gmail.com>
2020-11-18 07:04:38 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      name == 'more' && password == '0930'
     end
   end
 
