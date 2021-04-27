81b4f3a254a3f516e2ceefb01bc1738390b6f9d8
kamioka junichi <junichi1995930@gmail.com>
2021-04-27 22:49:07 +0900

    commit2

--- app/assets/javascripts/text-animation.js
+++ app/assets/javascripts/text-animation.js
@@ -1,15 +1,10 @@
 
 class TextAnimation {
     constructor(el) {
-      this.DOM = {};
-      this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
-      if (el instanceof HTMLElement) {
-        this.DOM.el = el;
-      } else {
-        this.DOM.el = document.querySelector(el);
-      }
-      this.chars = this.DOM.el.innerHTML.trim().split("");
-      this.DOM.el.innerHTML = this._splitText();
+        this.DOM = {};
+        this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
+        this.chars = this.DOM.el.innerHTML.trim().split("");
+        this.DOM.el.innerHTML = this._splitText();
     }
     _splitText() {
         return this.chars.reduce((acc, curr) => {
